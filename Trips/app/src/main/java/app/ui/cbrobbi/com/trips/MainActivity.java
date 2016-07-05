73635f332428061fcc329a4a52f884a4ad688949
chris <cbrobbi@gmail.com>
2016-07-05 14:51:28 -0500

    updates to main spinners, datepickers

--- Trips/app/src/main/java/app/ui/cbrobbi/com/trips/MainActivity.java
+++ Trips/app/src/main/java/app/ui/cbrobbi/com/trips/MainActivity.java
@@ -1,44 +1,73 @@
 package app.ui.cbrobbi.com.trips;
 
 
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.PopupWindow;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
     TextView travelBuddy;
     Button test1,test2;
     RadioButton button1, button2;
+    Spinner adltCntSpin, roomSpin, chldrnSpin;
+    private static final String [] numbCount = {"1","2","3","4","5"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout.activity_main);
+        setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,numbCount);
+        adltCntSpin = (Spinner) findViewById(R.id.adltCntSpin);
+        adltCntSpin.setAdapter(adapter);
+        chldrnSpin = (Spinner) findViewById(R.id.chldrnCntSpin);
+        chldrnSpin.setAdapter(adapter);
+        roomSpin = (Spinner) findViewById(R.id.roomCntSpin);
+        roomSpin.setAdapter(adapter);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+
         travelBuddy = (TextView) findViewById(R.id.travelBuddyText);
         travelBuddy.setOnClickListener(new View.OnClickListener(){
                                            @Override
@@ -59,10 +88,24 @@ public class MainActivity extends AppCompatActivity {
         });
         button1 = (RadioButton) findViewById(R.id.radioButton1);
         button2 = (RadioButton) findViewById(R.id.radioButton2);
+        dateView = (TextView) findViewById(R.id.textView301);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+
+
 
 
     }
-
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca", Toast.LENGTH_SHORT)
+                .show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -98,6 +141,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal)
+    {
+        Log.i("Main Numb Picker Adult", ""+newVal);
+    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this, myDateListener, year, month, day);
+        }
+        return null;
+    }
 
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int arg1, int arg2, int arg3) {
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2+1, arg3);
+        }
+    };
 
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
+    }
 }
+
+
